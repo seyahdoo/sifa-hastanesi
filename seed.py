091e003206ebaa0c31b78dfdf90965d9def51fb2
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-05-16 00:27:59 +0300

    doctor appointment

--- seed.py
+++ seed.py
@@ -8,8 +8,12 @@ if __name__ == "__main__":
 
     client.drop_database(DATABASE_NAME)
 
+    # patients
     add_user("ali","tester","ali@seyahdoo.com","123","05062605000","patient")
     add_user("veli","tester","veli@seyahdoo.com","123","05062600400","patient")
-    add_user("yılmaz","tester","yılmaz@seyahdoo.com","123","05062600300","doctor")
-    add_user("harici","tester","harici@seyahdoo.com","123","05062600100","doctor")
-    add_user("recai","deli","testerrecai@seyahdoo.com","123","05062604798")
+    add_user("recai","deli","recai@seyahdoo.com","123","05062604798")
+
+    # doctors
+    add_user("yılmaz","doktor","yılmaz@seyahdoo.com","123","05062600300","doctor")
+    add_user("hatice","hemşire","hatice@seyahdoo.com","123","05062600100","doctor")
+    add_user("vurdumduymaz","eleman","vurdumduymaz@seyahdoo.com","123","05062600100","doctor")
